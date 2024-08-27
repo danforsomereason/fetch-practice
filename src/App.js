91abed7f14a4c81e4996977562a1399195caabec
danforsomereason <147667936+danforsomereason@users.noreply.github.com>
2024-08-27 09:21:58 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pokemon, setPokemon] = useState(null)
+  useEffect(() => {
+    const myRequest = "https://pokeapi.co/api/v2/pokemon/1/";
+    fetch(myRequest).then(res => {
+      res.json().then((data) => setPokemon(data))      
+    }).catch(error => {
+      console.error(error)
+    })
+  }, [])
+  console.log(pokemon);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pokemon && (
+        <div>
+          <div>Name:</div>
+          {pokemon.name}
+          <div>Abilities:</div>
+          {pokemon.abilities.map((a) => {
+            return (<div>{a.ability.name}</div>)
+          })}
+          <div>Height:</div>
+          {pokemon.height}
+          <div>Weight:</div>
+          {pokemon.weight}
+        </div>
+      )}
     </div>
   );
 }
